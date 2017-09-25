55f7f987fb2479c784154f74e20dbbe9aacc2f30
Edgar Perez <e.perez@caribemedia.com.do>
2017-09-25 10:09:34 -0400

    Add project files.

---
+++ AllImagesInsideFoldersAndCopyToOtherFol/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Configuration;
+using System.IO;
+
+namespace AllImagesInsideFoldersAndCopyToOtherFol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "\\\\win-9lck90b4hfi\\ArtsPathPR";
+            ProcessDirectory(path);
+        }
+
+        
+        public static void ProcessDirectory(string targetDirectory)
+        {
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+            {
+                ProcessFile(fileName);
+            }
+            
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                ProcessDirectory(subdirectory);
+            }
+        }
+        
+        public static void ProcessFile(string path)
+        {
+            var Files = new FileInfo(path);
+            var filename = Files.Name;
+            System.IO.File.Copy(path, ConfigurationManager.AppSettings["URL_LOCAL"].ToString()+"\\"+ Files.Name, true);
+        }
+    }
+}
